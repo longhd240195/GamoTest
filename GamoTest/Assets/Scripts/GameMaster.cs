030feb1a6d0bceeac5a2fe8e6d43fffdd42ef1f8
Ares\Long <longhd240195@gmail.com>
2018-02-22 15:54:23 +0700

    - Add game master.

---
+++ GamoTest/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMaster : MonoBehaviour
+{
+	public static GameMaster gm;
+	public int highScore;
+	public int currentScore;
+
+	void Start ()
+	{
+		if (!gm) {
+			gm = this;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			DestroyImmediate (this.gameObject);
+		}
+	}
+
+	void OnEnable ()
+	{
+		LoadData ();
+	}
+
+	void OnDisable ()
+	{
+		if (gm == this)
+			SaveData ();
+	}
+
+	void LoadData ()
+	{
+		highScore = PlayerPrefs.GetInt ("highScore", 0);
+	}
+
+	void SaveData ()
+	{
+		PlayerPrefs.SetInt ("highScore", highScore);
+	}
+
+	public void LoadScene (string newScene)
+	{
+		SceneManager.LoadScene (newScene);
+	}
+}
---
+++ GamoTest/Assets/Scripts/StartScene/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+	public void btn_Quit ()
+	{
+		Application.Quit ();
+	}
+
+	public void btn_Play ()
+	{
+		GameMaster.gm.LoadScene ("PlayScene");
+	}
+}
